fa1c4169fc73b9e5aeb90850f5fff79e415f1c02
DinAish <124452091+DinAish@users.noreply.github.com>
2023-02-04 21:28:47 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		//Data type Variable Name = value; 
+		String mobileBrandName = "Redme";
+		char mobileLogo = '*';
+		int modelNumber = 3645;
+		long mobileEmiNumber = 49524566694466l;
+		float mobilePrice = 9999;
+		boolean isDamaged = true;
+		
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class TwoWheeler {
+	  public static void main(String[] args) {
+		  // Data type Variable Name = Value;
+		  
+		  int noOfMirrors = 2;
+		  short noOfWheels = 2;
+		  long chassisNumber = 589645666l;
+		  boolean isPunctured = false;
+		  String bikeName = "Yamaha";
+		  double runningKM =31555d;
+		  
+		  System.out.println(bikeName);
+		  System.out.println(noOfMirrors);
+		  
+		  
+	  }
+
+}
